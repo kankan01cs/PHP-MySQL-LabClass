9099648e3851f56065629ac461da98459942db47
kankan01cs <kankan01.cs@gmail.com>
2026-09-24 20:50:06 +0530

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Kankan";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First PHP Program</title>
+</head>
+<body>
+
+<h1>Hello, <?php echo $name; ?>!</h1>
+
+<p>This is my first PHP program on GitHub.</p>
+
+</body>
+</html>
